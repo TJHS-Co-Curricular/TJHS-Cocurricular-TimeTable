05d66113e238c519c0674ec91eacbe291b5682b6
ChongZhiJie <ChongZhiJie021604@gmail.com>
2026-04-27 16:31:15 +0800

    Repair TimeTable

--- script.js
+++ script.js
@@ -128,9 +128,9 @@ window.createTeacherRow = (teacher, isFirst, rowCount) => {
     
     // 07:14-08:00 Slots: Vertically merged for all teachers
     if (isFirst) {
-        html += `<td class="print vertical-text" rowspan="${rowCount}">早自习</td>`;
-        html += `<td class="print vertical-text" rowspan="${rowCount}">班导师时间</td>`;
-        html += `<td class="print vertical-text" rowspan="${rowCount}">晨读</td>`;
+        html += `<td class="print vertical-text" rowspan="${rowCount}">早 自 习 </td>`;
+        html += `<td class="print vertical-text" rowspan="${rowCount}">班 导 师 时 间 </td>`;
+        html += `<td class="print vertical-text" rowspan="${rowCount}">晨 读 </td>`;
     }
     
     html += `<td class="print-break"></td>`;
